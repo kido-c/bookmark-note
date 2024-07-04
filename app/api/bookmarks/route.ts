c7b9abd750f348d681d405b46c77d0b04d71398e
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-07-04 13:47:57 +0900

    temp: check post bookmark

--- app/api/bookmarks/route.ts
+++ app/api/bookmarks/route.ts
@@ -49,7 +49,10 @@ export async function POST(req: NextRequest) {
   try {
     const { url, name, tags, category, description }: PostBookmarkRequest =
       await req.json()
+
+    console.log(url, name)
     const verifiedUser = await getSession()
+    console.log(verifiedUser)
 
     if (!verifiedUser) {
       return NextResponse.json({ error: 'unauthorized' }, { status: 401 })
@@ -59,7 +62,7 @@ export async function POST(req: NextRequest) {
     const alreadyExsistUrl = await prisma.bookmark.findFirst({
       where: { url },
     })
-
+    console.log(alreadyExsistUrl)
     if (alreadyExsistUrl) {
       return NextResponse.json({ error: 'url already exists' }, { status: 400 })
     }
