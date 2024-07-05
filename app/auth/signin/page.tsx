f8286836d9f0d2bccddde3d6b45a30d2bfb49313
Ki Do <65141066+kido-c@users.noreply.github.com>
2024-07-05 15:23:11 +0900

    feat: signin validation 추가

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
+import { isEmail } from '@/app/utils/validation/isEmail'
 
 export interface SigninForm {
   email: string
@@ -11,8 +12,14 @@ export interface SigninForm {
 }
 
 export default function SigninPage() {
-  const { register, handleSubmit } = useForm<SigninForm>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SigninForm>()
+
   const router = useRouter()
+
   const onSubmit = (data: SigninForm) => {
     console.log(data)
     axios.post(`/api/auth/signin`, data).then(() => {
@@ -26,28 +33,43 @@ export default function SigninPage() {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-center items-center h-full w-full gap-3 px-8"
+      className="flex flex-col justify-center items-center h-full w-full px-8"
     >
-      <input
-        {...register('email')}
-        placeholder="이메일을 입력해주세요."
-        className="w-full h-12 rounded-lg p-3"
-      />
-      <input
-        {...register('password')}
-        placeholder="비밀번호를 입력해주세요."
-        className="w-full h-12 rounded-lg p-3"
-      />
+      <div className="w-full flex flex-col gap-1 h-20">
+        <input
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+            validate: (value) =>
+              isEmail(value) || '이메일 형식에 맞지 않습니다.',
+          })}
+          placeholder="이메일을 입력해주세요."
+          className="w-full h-12 rounded-lg p-3"
+        />
+        {errors.email && (
+          <p className="text-red-500 text-sm ">{errors.email.message}</p>
+        )}
+      </div>
+      <div className="w-full flex flex-col gap-1 h-20">
+        <input
+          type="password"
+          {...register('password', { required: '비밀번호를 입력해주세요.' })}
+          placeholder="비밀번호를 입력해주세요."
+          className="w-full h-12 rounded-lg p-3"
+        />
+        {errors.password && (
+          <p className="text-red-500 text-sm ">{errors.password.message}</p>
+        )}
+      </div>
       <div className="flex flex-col w-full mt-2 gap-3">
         <button
           type="submit"
-          className="bg-blue w-full h-12 rounded-lg  font-semibold"
+          className="bg-active text-white w-full h-12 rounded-lg  font-semibold"
         >
           로그인
         </button>
         <button
           type="button"
-          className="bg-blue w-full h-12 rounded-lg  font-semibold"
+          className=" border-active border text-main w-full h-12 rounded-lg  font-semibold"
         >
           회원 가입
         </button>
